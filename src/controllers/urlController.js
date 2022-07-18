4046ca46661fca9a789a204d0ce0e480665e2f6b
Ankita220296 <sanganiankita223@gmail.com>
2022-07-18 16:04:50 +0530

    2 APIs complete

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,8 +1,19 @@
 const urlModel = require("../models/urlModel");
-const validUrl = require("valid-url");
 const shortid = require("shortid");
 const axios = require("axios");
 
+// Validataion for empty request body
+const isValidObject = function (value) {
+  if (Object.keys(value).length === 0) return false;
+  else return true;
+};
+
+// Validation for Strings/ Empty strings
+const hasEmptyString = function (value) {
+  if (typeof value !== "string") return false;
+  else if (value.trim().length == 0) return false;
+  else return true;
+};
 const createShortUrl = async function (req, res) {
   try {
     let urlCode = shortid.generate();
@@ -13,41 +24,59 @@ const createShortUrl = async function (req, res) {
     data.urlCode = urlCode;
     data.shortUrl = shortUrl;
 
-    let found = false; // Using axios to check for correct Logolink with content type image
+    if (!isValidObject(data)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Missing Parameters" });
+    }
+    if (!longUrl) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Long url is mandatory" });
+    } else if (!hasEmptyString(longUrl)) {
+      return res
+        .status(400)
+        .send({ status: false, message: "Long url is in wrong format" });
+    }
+
+    let found = false; // Using axios to check for correct longurl
     await axios
       .get(longUrl)
       .then((response) => {
-        if (response.status == 200 || response.status == 201) {
-          if (response.headers["content-type"]) found = true;
-        }
+        if (response.status == 200 || response.status == 201) found = true;
       })
       .catch((error) => {});
 
     if (found == false) {
-      return res.status(400).send({ status: false, message: "wrong url" });
+      return res.status(400).send({ status: false, message: "Wrong url" });
     }
 
-    const validUrlCode = await urlModel.findOne({ urlCode: urlCode });
-    if (validUrlCode) {
+    const existUrlCode = await urlModel.findOne({ urlCode: urlCode });
+    if (existUrlCode) {
       return res
         .status(400)
         .send({ status: false, message: `${urlCode} is already exists` });
     }
-    const validShortUrl = await urlModel.findOne({ shortUrl: shortUrl });
-    if (validShortUrl) {
+    const existShortUrl = await urlModel.findOne({ shortUrl: shortUrl });
+    if (existShortUrl) {
       return res
         .status(400)
         .send({ status: false, message: `${shortUrl} is already exists` });
     }
-    const validLongUrl = await urlModel.findOne({ longUrl: longUrl });
-    if (validLongUrl) {
+    const existLongUrl = await urlModel.findOne({ longUrl: longUrl });
+    if (existLongUrl) {
       return res.status(400).send({
         status: false,
-        message: `${longUrl} is already exists so we can't generate urlcode`,
+        message: `${longUrl} is already exists so we can't generate urlcode with same url`,
       });
     }
-    const urlDetails = await urlModel.create(data);
-    return res.status(201).send({ status: true, data: urlDetails });
+    await urlModel.create(data);
+    const NewUrlDetails = {
+      longUrl: longUrl,
+      shortUrl: shortUrl,
+      urlCode: urlCode,
+    };
+    return res.status(201).send({ status: true, data: NewUrlDetails });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
@@ -56,16 +85,24 @@ const createShortUrl = async function (req, res) {
 const getUrl = async function (req, res) {
   try {
     let urlCode = req.params.urlCode;
+    // if (!shortid.isValid(urlCode)) {
+    //   return res.status(400).send({
+    //     status: false,
+    //     message: `${urlCode} is not valid`,
+    //   });
+    // }
     const originalUrl = await urlModel
       .findOne({ urlCode: urlCode })
       .select({ longUrl: 1, _id: 0 });
 
     if (originalUrl) {
-       res.status(302).send({
-        status: true,
-        message: `Redirection to ${originalUrl.longUrl}`,
+      res.redirect(originalUrl.longUrl);
+    }
+    if (!originalUrl) {
+      return res.status(400).send({
+        status: false,
+        message: "Url not found",
       });
-      return res.redirect(302, originalUrl.longUrl);
     }
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
