0db9003043fd2b11ace1773659e108210713dd7f
Ankita220296 <sanganiankita223@gmail.com>
2022-07-19 11:01:52 +0530

    Little Changes

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -14,6 +14,8 @@ const isEmpty = function (value) {
   else if (value.trim().length == 0) return false;
   else return true;
 };
+
+//......................................... Create Short Url ......................................//
 const createShortUrl = async function (req, res) {
   try {
     let data = req.body;
@@ -23,11 +25,7 @@ const createShortUrl = async function (req, res) {
         .send({ status: false, message: "Missing Parameters" });
     }
     let longUrl = data.longUrl;
-    let urlCode = shortid.generate();
     const baseUrl = "http://localhost:3000/";
-    const shortUrl = baseUrl.concat(urlCode);
-    data.urlCode = urlCode;
-    data.shortUrl = shortUrl;
 
     if (!longUrl) {
       return res
@@ -51,44 +49,43 @@ const createShortUrl = async function (req, res) {
       return res.status(400).send({ status: false, message: "Wrong url" });
     }
 
-    const isUrlExists = await urlModel.findOne({ urlCode: urlCode });
-    if (isUrlExists) {
-      return res
-        .status(400)
-        .send({ status: false, message: `${urlCode} is already exists` });
-    }
-    const isShortUrlExists = await urlModel.findOne({ shortUrl: shortUrl });
-    if (isShortUrlExists) {
-      return res
-        .status(400)
-        .send({ status: false, message: `${shortUrl} is already exists` });
-    }
     const isLongUrlExists = await urlModel
       .findOne({ longUrl: longUrl })
       .select({ urlCode: 1, longUrl: 1, shortUrl: 1, _id: 0 });
+
     if (isLongUrlExists) {
-      return res.status(400).send({
-        status: false,
+      return res.status(200).send({
+        status: true,
         message: "Short url is already generated",
         data: isLongUrlExists,
       });
     }
+    const urlCode = shortid.generate();
+    const shortUrl = baseUrl.concat(urlCode);
+    data.urlCode = urlCode;
+    data.shortUrl = shortUrl;
+
     await urlModel.create(data);
-    const NewUrlDetails = {
-      longUrl: longUrl,
-      shortUrl: shortUrl,
-      urlCode: urlCode,
-    };
-    return res.status(201).send({ status: true, data: NewUrlDetails });
+    return res
+      .status(201)
+      .send({ status: true, data: { longUrl, shortUrl, urlCode } });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
 };
 
+//.........................................Get Url ......................................//
 const getUrl = async function (req, res) {
   try {
     let urlCode = req.params.urlCode;
 
+    if (!shortid.isValid(urlCode)) {
+      //checks for space and special character or length less than 7
+      return res.status(400).send({
+        status: false,
+        message: "Invalid url code",
+      });
+    }
     const originalUrl = await urlModel
       .findOne({ urlCode: urlCode })
       .select({ longUrl: 1, _id: 0 });
