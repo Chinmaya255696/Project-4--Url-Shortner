77913309478ccb505d92a075a3a9089602af96e5
chinmaya255696 <chinmayasahoo.v@gmail.com>
2022-07-21 23:38:25 -0700

    update

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -7,17 +7,21 @@ const { promisify } = require("util");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  13665,
-  "redis-13665.c212.ap-south-1-1.ec2.cloud.redislabs.com",
-  { no_ready_check: true }
+  13665, //port number
+  "redis-13665.c212.ap-south-1-1.ec2.cloud.redislabs.com",// one type of cluster name
+  { no_ready_check: true } 
 );
 redisClient.auth("Id1Km5GkgaOYSwoOFluI4oAvomkz91Iw", function (err) {
   if (err) throw err;
 });
 
-redisClient.on("connect", async function () {
+redisClient.on("connect", async function () { 
   console.log("Connected to Redis..");
 });
+// redisClient.flushdb( function (err, succeeded) {
+//   console.log(succeeded); // will be true if successfull
+// });
+
 
 //Connection setup for redis
 
@@ -43,30 +47,27 @@ const createShortUrl = async function (req, res) {
     let data = req.body;
 
     if (!checkBodyParams(data)) {
-      return res
-        .status(400)
+      return res.status(400)
         .send({ status: false, message: "Missing Parameters" });
     }
     let longUrl = data.longUrl;
     const baseUrl = "http://localhost:3000/";
 
     if (!longUrl) {
-      return res
-        .status(400)
+      return res.status(400)
         .send({ status: false, message: "Long url is mandatory" });
     } else if (!isEmpty(longUrl)) {
-      return res
-        .status(400)
+      return res.status(400)
         .send({ status: false, message: "Long url is in wrong format" });
     }
 
     let found = false; // Using axios to check for correct longurl
-    await axios
-      .get(longUrl)
-      .then((response) => {
+    await axios.get(longUrl)
+      .then((response) => {  //console.log(response)
         if (response.status == 200 || response.status == 201) found = true;
       })
       .catch((err) => {});
+      
 
     if (!found) {
       return res.status(400).send({ status: false, message: "Wrong url" });
@@ -76,13 +77,10 @@ const createShortUrl = async function (req, res) {
 
     if (cahcedProfileData) {
       data = JSON.parse(cahcedProfileData);
-      return res
-        .status(200)
-        .send({
+      return res.status(200).send({
           status: true,
-          message: "Short url already generated",
-          data: data,
-        });
+          message: "Short url already generated and it is coming from caching.",
+          data: data,});
     } else {
       const urlCode = shortid.generate();
       const shortUrl = baseUrl.concat(urlCode);
@@ -94,9 +92,7 @@ const createShortUrl = async function (req, res) {
       if (!existLongUrl) {
         await urlModel.create(data);
       } else {
-        return res
-          .status(200)
-          .send({
+        return res.status(200).send({
             status: true,
             message: "Short url already generated",
             data: data,
@@ -104,9 +100,7 @@ const createShortUrl = async function (req, res) {
       }
       await SET_ASYNC(`${req.body.longUrl}`, JSON.stringify({ data }));
     }
-    return res
-      .status(201)
-      .send({ status: true, message: "Sucess", data: data });
+    return res.status(201).send({ status: true, message: "Sucess", data: data });
   } catch (error) {
     return res.status(500).send({ status: false, message: error.message });
   }
